2e9b884f5cde0cfa7119380346a3309ac49a3d29
Ben Reiner <benjamin.t.reiner@gmail.com>
2022-04-26 09:40:02 -0400

    Read weather API

---
+++ functions/app.js
@@ -0,0 +1,5 @@
+import weather from "./weather.js";
+
+let bostonWeather = weather.getCurrentWeather("boston");
+
+console.log(bostonWeather);
---
+++ functions/weather.js
@@ -0,0 +1,46 @@
+const weather = (() => {
+  const _apiKey = "bdecf9b86e53f1f1ab5da9ef4c076621";
+
+  // Get current weather data from the API
+  // We care about the city name, temperatures, wind speeds and weather status
+  // (clear, cloudy, rain, snow...)
+  function formatCurrentWeatherData(data) {
+    let formatted = {
+      cityName: data.name,
+      temperature: {
+        current: data.main.temp,
+        humidity: data.main.humidity,
+        feelsLike: data.main.feels_like,
+      },
+      wind: {
+        speed: data.wind.speed,
+        direction: data.wind.deg,
+      },
+      weather: {
+        id: data.weather[0].id,
+        type: data.weather[0].main,
+      },
+    };
+
+    return formatted;
+  }
+
+  async function getCurrentWeather(city) {
+    const endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${_apiKey}`;
+    try {
+      const response = await fetch(endpoint, { mode: "cors" });
+      if (!response.ok) throw new Error(`City ${city} not found`);
+      const formattedData = formatCurrentWeatherData(await response.json());
+      return formattedData;
+    } catch (error) {
+      alert(error);
+      return null;
+    }
+  }
+
+  // Get the 7-day forecast for the given city.
+
+  return { getCurrentWeather };
+})();
+
+export default weather;
